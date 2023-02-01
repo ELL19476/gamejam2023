7b51c63160809b75d13d78a05b4a643342dbd124
Sami Ellmauer <ELL19476@spengergasse.at>
2023-02-01 20:20:29 +0100

    Character Controller

--- ThroughTheAges/Assets/Logic/Mover.cs
+++ ThroughTheAges/Assets/Logic/Mover.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     protected Rigidbody rigidBody;
-    protected CapsuleCollider col;
+    protected CapsuleCollider capsule;
     protected Rigidbody[] childBodies;
     [Header("SetUp")]  
    [SerializeField] 
@@ -28,13 +28,15 @@ public class Mover : MonoBehaviour
     protected float airControl;
 
     protected Vector3 accumulatedVel = Vector3.zero;
-    private Vector3 lastVelocity = Vector3.zero;
+    protected Vector3 lastVelocity = Vector3.zero;
 
+    bool grounded = false;
+    Vector3 lastColDir = Vector3.up;
 
     protected virtual void Start() {
         rigidBody = GetComponent<Rigidbody>();
         // rigidBody.interpolation = RigidbodyInterpolation.None;
-        col = GetComponent<CapsuleCollider>();
+        capsule = GetComponent<CapsuleCollider>();
         rigidBody.freezeRotation = true;
 
         List<Rigidbody> rbs = GetComponentsInChildren<Rigidbody>().ToList();
@@ -49,57 +51,89 @@ public class Mover : MonoBehaviour
         EnableRagdoll(false);
     }
     protected void EnableRagdoll(bool enable) {
-        col.enabled = !enable;
+        capsule.enabled = !enable;
         foreach (Rigidbody rb in childBodies) {
             rb.isKinematic = !enable;
             rb.detectCollisions = enable;
         }
     }
-    protected bool IsGrounded(out RaycastHit hit) {
-        float downVelocity = Vector3.Dot(accumulatedVel, Vector3.down);
-        if(downVelocity < 0) {
-            hit = new RaycastHit();
+    protected bool IsGrounded(out Vector3 dist) {
+        dist = Vector3.zero;
+        if(Vector3.Dot(accumulatedVel, Vector3.down) < 0)
             return false;
+
+        Vector3 lowerCapsuleSphereCenter = transform.TransformPoint(capsule.center) - transform.up * (capsule.height / 2f - capsule.radius);
+        Vector3 groundPos = lowerCapsuleSphereCenter - transform.up * (capsule.radius - 0.05f);
+        Collider[] cols = Physics.OverlapSphere(lowerCapsuleSphereCenter, capsule.radius + 0.05f, 1 << 6);
+        if(cols.Length > 0) {
+            int i = 0;
+            if(cols.Length > 1) {
+                Physics.Raycast(groundCheck.position, -transform.up, out RaycastHit grnd, 1f, 1 << 6);
+                for(int j = 0; j < cols.Length; j++) {
+                    if(cols[j].gameObject == grnd.collider.gameObject) {
+                        i = j;
+                        break;
+                    }
+                }
+            }
+            Vector3 p = cols[i].ClosestPoint(groundPos);
+            dist = (groundPos - p);
+            lastColDir = dist;
+            return true;
         }
-        Vector3 o = Vector3.up * (col.height / 2f - col.radius - 0.1f);
-        return Physics.CapsuleCast(transform.TransformPoint(col.center) + o, transform.TransformPoint(col.center) - o, col.radius, Vector3.down, out hit, downVelocity * Time.deltaTime + 0.1f, 1 << 6);
+
+        return false;
     }
-    protected bool CanMove(Vector3 normal) {
+    protected virtual bool CanMove(Vector3 normal) {
         return Physics.Raycast(groundCheck.position, -normal, out _, 1f, 1 << 6);
     }
-    protected virtual bool MoveTowards(Vector3 target)
+    protected virtual bool MoveTowards(Vector3 target, bool slide = false)
     {
         Vector3 velocity = Vector3.zero;
         bool canMove = true;
-        if(IsGrounded(out RaycastHit hit)) {
+        if(IsGrounded(out Vector3 dist)) {
+            Vector3 normal = dist.normalized;
             // Get the direction to the target
-            Vector3 direction = Vector3.Cross(hit.normal, Vector3.forward);
+            Vector3 direction = Vector3.Cross(normal, Vector3.forward);
             float dot = Vector3.Dot(target - transform.position, direction);
+            direction *= Mathf.Sign(dot); 
             if(Mathf.Abs(dot) > stoppingDist) {   
-                direction *= Mathf.Sign(dot); 
-                if(CanMove(hit.normal))
+                if(CanMove(normal))
                     // Move towards the target
                     velocity = direction * speed;
                 else {
                     canMove = false;
                 }
 
-                // Rotate towards the target
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 3.5f * speed * Time.deltaTime);
             }
+            // Rotate towards the target
+            rigidBody.MoveRotation(Quaternion.Slerp(rigidBody.rotation, Quaternion.LookRotation(direction), 3.5f * speed * Time.deltaTime));
+            
             lastVelocity = velocity;
-            velocity += accumulatedVel;
-            accumulatedVel = Vector3.zero;
+            if(!grounded) {
+                // Move to ground
+                grounded = true;
+            }
+            if(slide) {
+                // Slide down slopes
+                AddGravity();
+                accumulatedVel = Vector3.ProjectOnPlane(accumulatedVel, normal);
+            } else {
+                // Stop Vertical Velocity
+                accumulatedVel = Vector3.zero;
+            }
         } else {
+            grounded = false;
             // Momentum
             lastVelocity -= lastVelocity * Mathf.Clamp01(airDrag * Time.deltaTime);
             velocity = lastVelocity;
             velocity = Vector3.ClampMagnitude(velocity, Mathf.Abs(maxAirSpeed));
-            velocity += (target - transform.position).normalized * speed * airControl;
+            if(CanMove(Vector3.up)) 
+                velocity += (target - transform.position).normalized * speed * airControl;
+
             velocity = Vector3.ClampMagnitude(velocity, Mathf.Abs(maxAirSpeed));
+            AddGravity();
             canMove = false;
-            // Gravity
-            accumulatedVel += Time.deltaTime * gravityScale * Physics.gravity;
             // Rotate towards the target
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right * Mathf.Sign(target.x - transform.position.x)), 3.5f * speed * Time.deltaTime);
         }
@@ -107,9 +141,13 @@ public class Mover : MonoBehaviour
         return canMove;
     }
 
-    protected void Jump() {
-        if(IsGrounded(out RaycastHit hit)) {
-            accumulatedVel += Vector3.up * jumpSpeed;
-        }
+    protected void AddGravity() {
+        // Gravity
+        accumulatedVel += Time.deltaTime * gravityScale * Physics.gravity;
+    }
+
+    protected virtual void Jump() {
+        accumulatedVel.y = 0;
+        accumulatedVel += Vector3.up * jumpSpeed;
     }
 }
--- ThroughTheAges/Assets/Logic/Player.cs
+++ ThroughTheAges/Assets/Logic/Player.cs
@@ -9,27 +9,89 @@ public class Player : Mover
     bool fastFalling = false;
     bool slowJump = false;
 
+    float lastGroundedTime = 0f;
+
+    // JUMP BUFFERING
+    KeyCode inputBuffer; 
+    float lastInputBufferTime = 0.2f;
+
+    [Header("Player Movement")]
+    [SerializeField, Range(0, 3)]
+    protected float ballAcceleration = 1f;
+    [SerializeField, Range(0, 100)]
+    protected float ballDrag = 1f;
+    [SerializeField, Range(0, 100)]
+    protected float maxBallSpeed = 1f;
+    [SerializeField]
+    protected float ballMass = 10;
+
     [Header("Juice")]
     [Range(0, 1)]
     public float slowJumpFactor = 0.5f;
     [Range(0, 7)]
     public float fastFallFactor = 4f;
 
+
+
+    [Header("Helpers")]
+    [SerializeField]
+    protected float coyoteTime = 0.1f;
+    [SerializeField]
+    protected float bufferTime = 0.2f;
+
+    public enum PlayerState {
+        Baby,
+        Teen,
+        Old
+    }
+    // [HideInInspector]
+    [Header("TMP")]
+    public PlayerState state;
+
+    bool _ball = false;
+    bool ballEnabled {
+        get {
+            return _ball;
+        }
+        set {
+            _ball = value;
+            EnableBall(value);
+        }
+    }
+    int dir = 1;
+
+    protected SphereCollider ballCollider;
+    float normalMass = 1f;
+
     protected override void Start() {
         base.Start();
+        ballCollider = GetComponent<SphereCollider>();
+        ballEnabled = false;
+        normalMass = rigidBody.mass;
+
         originalGravity = gravityScale;
     }
 
     private void Update() {
-        MoveTowards(transform.position + Vector3.right * Input.GetAxisRaw("Horizontal"));
-        if(Input.GetKeyDown(KeyCode.Space)) {
-            Jump();
-            gravityScale *= slowJumpFactor;
-            slowJump = true;
-        } else if(slowJump && (Input.GetKeyUp(KeyCode.Space) || Vector3.Dot(accumulatedVel, Vector3.up) < 0)) {
-            gravityScale = originalGravity;
-            slowJump = false;
+        if(inputBuffer != KeyCode.None && Time.time - lastInputBufferTime > bufferTime) {
+            inputBuffer = KeyCode.None;
         }
+        if(IsGrounded(out _)) {
+            lastGroundedTime = Time.time;
+        }
+        DoActions(inputBuffer);
+    }
+
+    protected void DoActions(KeyCode bufferedInput) {
+        float h = Input.GetAxisRaw("Horizontal");
+        if(!ballEnabled && h != 0) {
+            dir = h > 0 ? 1 : -1;
+        }
+        if(!ballEnabled) {
+            MoveTowards(transform.position + Vector3.right * dir);
+        }
+
+        SpecialAction(bufferedInput);
 
         if(!fastFalling && Input.GetAxisRaw("Vertical") < 0 && Vector3.Dot(accumulatedVel, Vector3.up) < 0) {
             gravityScale *= fastFallFactor;
@@ -38,6 +100,83 @@ public class Player : Mover
             gravityScale = originalGravity;
             fastFalling = false;
         }
+    }
+    protected void SpecialAction(KeyCode bufferedInput) {
+        if(state == PlayerState.Teen) {
+            if(Input.GetKeyDown(KeyCode.Space) || bufferedInput == KeyCode.Space) {
+                Jump();
+                if(bufferedInput == KeyCode.Space) {
+                    inputBuffer = KeyCode.None;
+                }
+                if(Input.GetKey(KeyCode.Space)) {
+                    gravityScale *= slowJumpFactor;
+                    slowJump = true;
+                }
+            } else if(slowJump && (Input.GetKeyUp(KeyCode.Space) || Vector3.Dot(accumulatedVel, Vector3.up) < 0)) {
+                gravityScale = originalGravity;
+                slowJump = false;
+            }
+        } else if(state == PlayerState.Baby) {
+            if(Input.GetKeyDown(KeyCode.Space) || bufferedInput == KeyCode.Space) {
+                ballEnabled = true;
+            } 
+            if(ballEnabled && Input.GetKeyUp(KeyCode.Space)) {
+                ballEnabled = false;
+            }
+        } else if(state == PlayerState.Old) {
+            // Attack
+        }
+    }
 
+    protected virtual void EnableBall(bool enable) {
+        rigidBody.freezeRotation = !enable;
+        capsule.enabled = !enable;
+        ballCollider.enabled = enable;
+        if(!enable) {
+            rigidBody.mass = normalMass;
+            if(IsGrounded(out _)) {
+                rigidBody.MoveRotation(Quaternion.LookRotation(Vector3.up));
+                accumulatedVel = Vector3.up * jumpSpeed;
+            }
+        }
+        else {
+            rigidBody.mass = ballMass;
+            if(IsGrounded(out Vector3 dist)) {
+                Vector3 tangent = Vector3.Cross(dist.normalized, Vector3.forward);
+                lastVelocity = tangent * dir * speed * ballAcceleration;
+            } else {
+                // TODO
+            }
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        if(!ballEnabled) return;
+        Vector3 acceleration = Physics.gravity * gravityScale;
+        rigidBody.AddForce(acceleration, ForceMode.Acceleration);
+
+        lastVelocity -= lastVelocity * Mathf.Clamp01(ballDrag * Time.fixedDeltaTime);
+        Debug.Log(lastVelocity);
+        // additional acceleration
+        rigidBody.velocity += lastVelocity;
+        // clamp speed
+        rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, maxBallSpeed);
+
+    }
+
+    protected override void Jump() {
+        if(Time.time - lastGroundedTime < coyoteTime) {
+            base.Jump();
+        } else {
+            // Buffer jump
+            inputBuffer = KeyCode.Space;
+            lastInputBufferTime = Time.time;
+        }
+    }
+
+    protected override bool CanMove(Vector3 normal)
+    {
+        return Input.GetAxisRaw("Horizontal") != 0;
     }
 }
