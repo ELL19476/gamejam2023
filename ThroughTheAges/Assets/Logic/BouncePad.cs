581ea21aab1f9b1b5aa874e9c3abd65344155534
Sami Ellmauer <ELL19476@spengergasse.at>
2023-02-02 12:25:30 +0100

    Cinemachine

--- ThroughTheAges/Assets/Logic/BouncePad.cs
+++ ThroughTheAges/Assets/Logic/BouncePad.cs
@@ -7,11 +7,15 @@ public class BouncePad : MonoBehaviour
     [SerializeField] float force = 2f;
     [SerializeField] float minVelocity = 7.5f;
     private void OnCollisionEnter(Collision other) {
-        if(other.gameObject.TryGetComponent(out Mover mover)) {
-            mover.SetVelocity(transform.up * 
+        Vector3 f = transform.up * 
             Mathf.Max(force * (other.relativeVelocity.magnitude) * (other.rigidbody? 1f / other.rigidbody.mass : 1),
                 minVelocity
-            ));
+            );
+        if(other.gameObject.TryGetComponent(out Mover mover)) {
+            mover.SetVelocity(f);
+        } else if(other.rigidbody){
+            other.rigidbody.velocity = Vector3.zero;
+            other.rigidbody.AddForce(f, ForceMode.VelocityChange);
         }
     }
 }
