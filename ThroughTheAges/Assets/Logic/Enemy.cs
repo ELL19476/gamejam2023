50ca0e4fa3b9a8ebc838540c3991f4c27d0de179
Alwin Zitek <alwinzit@gmail.com>
2023-02-02 13:43:34 +0100

    awlin

---
+++ ThroughTheAges/Assets/Logic/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Enemy : Mover
+{
+    Vector3 t = Vector3.zero;
+    bool moveToTarget = false;
+
+    protected override void Start() {
+        base.Start();
+        SetTarget(transform.position + Vector3.right * 5f);
+    }
+
+    public void SetTarget(Vector3 target) {
+        t = target;
+        moveToTarget = true;
+    }
+    private void Update() {
+        moveToTarget = MoveTowards(moveToTarget?t:transform.position);
+    }
+}
